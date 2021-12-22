4eb6724c2b6a7a3639439e0bc71c230874148f6f
GeorgeCh2 <georgechmr@gmail.com>
2021-12-22 18:00:08 +0800

    update leetcode 91-Decode Ways

--- study/leetcode/DynamicProgramming/DecodeWays.java
+++ study/leetcode/DynamicProgramming/DecodeWays.java
@@ -5,25 +5,26 @@
  */
 public class DecodeWays {
 	public int numDecodings(String s) {
-		// ways[s.length()+1] 代表所有的解码方法
-		int[] ways = new int[s.length()+1];
-		ways[0] = 1;
+		if ('0' == s.charAt(0)) {
+			return 0;
+		}
 
-		// ways[i] = ways[i-1] (使用一个数字)
-		// ways[i] = ways[i-2] (使用2个数字)
-		char[] chars = s.toCharArray();
+		int oneNumber = 1, twoNumber = 0, ans = 0;
 		for (int i = 1; i < s.length()+1; i++) {
+			ans = 0;
 			// 使用1个数字
-			if ('0' != chars[i-1]) {
-				ways[i] += ways[i-1];
+			if ('0' != s.charAt(i - 1)) {
+				ans += oneNumber;
 			}
 
 			// 组合2个数字
-			if (i > 1 && '0' != chars[i-2] && ((chars[i-2] - '0') * 10 + (chars[i-1] - '0') <= 26)) {
-				ways[i] += ways[i-2];
+			if (i > 1 && '0' != s.charAt(i - 2) && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
+				ans += twoNumber;
 			}
+			twoNumber = oneNumber;
+			oneNumber = ans;
 		}
 
-		return ways[s.length()];
+		return ans;
 	}
 }
